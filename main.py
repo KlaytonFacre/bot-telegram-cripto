691f642fedc27206ef63f38e77bcb81838ed01b6
Klayton Facre <klayton.facre@hotmail.com>
2023-02-13 00:57:39 -0300

    update 13 fev 2023
    Updated to use package python-dotenv to store telegram sensitive data.

--- main.py
+++ main.py
@@ -1,32 +1,21 @@
 import telegram
-import asyncio
 import locale
-from time import sleep
-from modules.TelegramData import TelegramData
+import os
 from modules.CoinGeckoAPI import CoinGeckoAPI
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
-
-"""
-proxies = {
-  'http': 'http://192.168.132.202:8080',
-  'https': 'http://192.168.132.202:8080',
-}
-"""
-
 
 # Define variáveis e constantes utilizadas no programa
 # Conjunto de moedas de interesse
 moedas = {'bitcoin', 'ethereum'}
 
-# Dados para a conexão com o Bot do Telegram que virão do arquivo
-telegram_data = TelegramData(nome='cripto_jah')
-telegram_data.carregar_dados_de('./telegram_data.txt')
-
 # Cria um Objeto Bot do pacote telegram
-bot = telegram.Bot(token=telegram_data.get_token())
+bot = telegram.Bot(token=str(os.getenv('TELEGRAM_TOKEN')))
 
 for moeda in moedas:
     preco, timestamp = CoinGeckoAPI.get_precos(moeda)
-    asyncio.run(bot.send_message(text=f'Cotação de {moeda}: R$ {preco} em {timestamp}', chat_id=telegram_data.get_chat_id()))
-    sleep(10)
+    bot.send_message(text=f'Cotação de {moeda}: R$ {preco} em {timestamp}', chat_id=str(os.getenv('TELEGRAM_CHAT_ID')))
